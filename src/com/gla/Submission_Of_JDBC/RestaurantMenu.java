5f47939aefcc2bb2dab11b2c701528d42446c62c
Varun a <varunakash65@gmail.com>
2026-04-25 21:19:08 +0530

    Solution

---
+++ src/com/gla/Submission_Of_JDBC/RestaurantMenu.java
@@ -0,0 +1,19 @@
+package src.com.gla.Submission_Of_JDBC;
+import java.sql.*;
+
+public class RestaurantMenu {
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testdb","root","password");
+
+            Statement st = con.createStatement();
+
+            System.out.println("Connected Successfully");
+
+            con.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
